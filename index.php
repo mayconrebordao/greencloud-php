5170aded3e667a3d6bc9c64ea818af98773c0791
mayconrebordao <r.maycon1122@gmail.com>
2017-11-24 16:08:17 -0200

    update ..

--- index.php
+++ index.php
@@ -5,8 +5,17 @@
 				<div class="mdl-shadow--2dp mdl-cell " style="width: 100%;">
 				<h1 class="mdl-cardmdl-card__title-text mdl-shadow--2dp " style="text-align: center;width: 100%;margin: 0px;">Bem Vindo!</h1>
 
-					<div class="mdl-grid">
-
+					<div class="mdl-grid center-items">
+						<div class="mdl-cell mdl-cell--8-col-desktop mdl-cell--4-col-phone mdl-cell--6-col-tablet mdl-shadow--2dp">
+							<div class="mdl-card mdl-js-card">
+								<div class="mdl-card__title">
+									<div class="mdl-card__title-tex">Olá, seja bem-vindo ao GreenCloud</div>
+								</div>
+							</div>
+							<div class="mdl-card__supporting-text">
+								
+							</div>
+						</div>
 
 
 
