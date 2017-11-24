1973f3e24dcd99ca6970e9bf594409b1f5955ec4
mayconrebordao <r.maycon1122@gmail.com>
2017-11-24 17:14:40 -0200

    update ..

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once('_head.php'); ?>
 		<main class="mdl-layout__content">
-			<div class="mdl-grid center-items " style="height: 100%; min-height: 600px;">
+			<div class="mdl-grid center-items " style="height: 100%; ">
 				
 				<div class="mdl-shadow--2dp mdl-cell " style="width: 100%;">
 				<h1 class="mdl-cardmdl-card__title-text mdl-shadow--2dp " style="text-align: center;width: 100%;margin: 0px;">Bem Vindo!</h1>
